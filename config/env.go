65883f0c6fb044ff3d31d1bd0da0b8607dbf23a6
Chirag Sharma <chirag.sharma@teqexpert.com>
2024-06-27 12:50:06 +0530

    done

--- config/env.go
+++ config/env.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -9,11 +10,15 @@ import (
 func LoadENV() error {
 	goEnv := os.Getenv("GO_ENV")
 	if goEnv == "" || goEnv == "development" {
-		err := godotenv.Load()
-		if err != nil {
-			return err
+		if _, err := os.Stat(".env"); err == nil {
+			// .env file exists, load it
+			if loadErr := godotenv.Load(); loadErr != nil {
+				return fmt.Errorf("Error loading .env file: %w", loadErr)
+			}
+		} else {
+			// .env file does not exist, log or handle it as needed
+			fmt.Println("No .env file found, skipping loading .env file")
 		}
 	}
-
 	return nil
 }
