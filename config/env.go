7f39d5b63a036d6a12329d12a68db39221120f4f
Chirag Sharma <chirag.sharma@teqexpert.com>
2024-07-02 10:14:46 +0530

    done

--- config/env.go
+++ config/env.go
@@ -1,19 +1,19 @@
 package config
 
-import (
-	"os"
+// import (
+// 	"os"
 
-	"github.com/joho/godotenv"
-)
+// 	"github.com/joho/godotenv"
+// )
 
-func LoadENV() error {
-	goEnv := os.Getenv("GO_ENV")
-	if goEnv == "" || goEnv == "development" {
-		err := godotenv.Load()
-		if err != nil {
-			return err
-		}
-	}
+// func LoadENV() error {
+// 	goEnv := os.Getenv("GO_ENV")
+// 	if goEnv == "" || goEnv == "development" {
+// 		err := godotenv.Load()
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
