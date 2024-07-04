72b8c63e64b62073a54e0b0dcddea82b3927b590
Chirag Sharma <chirag.sharma@teqexpert.com>
2024-07-04 11:27:46 +0530

    done

--- entity/userEntity.go
+++ entity/userEntity.go
@@ -7,14 +7,14 @@ import (
 )
 
 type CustomerEntity struct {
-	Id             primitive.ObjectID `json:"id" bson:"_id"`
-	UserName       string             `json:"userName" bson:"userName"`
-	Email          string             `json:"email" bson:"email"`
-	Password       string             `json:"password" bson:"password"`
-	SocialDetails  SocialDetails      `json:"socialDetails" bson:"socialDetails"`
-	SessionId string             `json:"sessionId" bson:"sessionId"`
-	CreatedAt      time.Time          `json:"cresatedAt" bson:"createdAt"`
-	UpdatedAt      time.Time          `json:"updatedAt" bson:"updatedAt"`
+	Id            primitive.ObjectID `json:"id" bson:"_id"`
+	UserName      string             `json:"userName" bson:"userName"`
+	Email         string             `json:"email" bson:"email"`
+	Password      string             `json:"password" bson:"password"`
+	SocialDetails SocialDetails      `json:"socialDetails" bson:"socialDetails"`
+	SessionId     string             `json:"sessionId" bson:"sessionId"`
+	CreatedAt     time.Time          `json:"cresatedAt" bson:"createdAt"`
+	UpdatedAt     time.Time          `json:"updatedAt" bson:"updatedAt"`
 }
 
 type SocialDetails struct {
