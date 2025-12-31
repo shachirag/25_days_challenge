6f55fe678479c7290057f1454847095603837ffa
Chirag Sharma <chiragsharma75757@gmail.com>
2025-12-31 11:01:32 +0530

    completed

---
+++ database/aws.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/ses"
+)
+
+var (
+	sesClient *ses.Client
+	s3Client  *s3.Client
+)
+
+func SetupAWSClient() error {
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Printf("error: %v", err)
+		return err
+	}
+	s3Client = s3.NewFromConfig(cfg)
+	sesClient = ses.NewFromConfig(cfg)
+
+	return nil
+}
+
+func GetS3Uploader() *manager.Uploader {
+	return manager.NewUploader(s3Client)
+}
+
+func GetSesClient() *ses.Client {
+	return sesClient
+}
